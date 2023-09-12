cfa7ac9ba7724e2096ed7b04af992f357cf6d00e
José Messias <josecmessias22@gmail.com>
2023-09-12 20:31:24 -0300

    Correção da página de editar

--- algCar/src/pages/Editar/EditarVeiculos.jsx
+++ algCar/src/pages/Editar/EditarVeiculos.jsx
@@ -1,15 +1,16 @@
 import { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
+import './EditarVeiculos.css';
 
 export const EditarVeiculos = () => {
     const { index } = useParams();
     const navigate = useNavigate();
-    const [veiculo, setveiculo] = useState({ marca: '', modelo: ''});
+    const [veiculo, setVeiculo] = useState({ marca: '', modelo: ''});
 
     useEffect(() => {
         const storedveiculo = JSON.parse(localStorage.getItem('veiculos')) || [];
         if(storedveiculo[index]){
-            setveiculo(storedveiculo[index]);
+            setVeiculo(storedveiculo[index]);
         }
     }, [index]);
 
@@ -24,31 +25,69 @@ export const EditarVeiculos = () => {
         navigate('/');
     }
 
-
     return (
-        <div>
-            <h2>Editar Veículo</h2>
-            <form onSubmit={handleSubmit}>
-                <label>
-                    Marca:
-                </label>
-                <input 
-                    type="text" 
-                    value={veiculo.marca} 
-                    onChange={(e) => setveiculo({ ...veiculo, marca: e.target.value})} 
-                />
-                <br/>
-                <label>
-                    Modelo:
-                </label>
-                <input 
-                    type="text" 
-                    value={veiculo.modelo} 
-                    onChange={(e) => setveiculo({ ...veiculo, modelo: e.target.value})} 
-                />
-                <br/>
-                <button type="submit">Salvar alterações</button>
-            </form>
+            <div className="form-editar">
+                <div className="form-container-editar">
+                    <h2 className="Titulo-editar"> Editar Veículo </h2>
+                    <form onSubmit={handleSubmit} >
+                        <div className="form-inputs-editar">
+                            <div className="editar-grupo">
+                                <label className="form-label-editar">
+                                    Marca:  
+                                    <input type="text" value={veiculo.marca} onChange={(e) => setVeiculo({ ...veiculo, marca: e.target.value})}/>
+                                </label>
+
+                                <label className="form-label-editar">
+                                    Modelo:
+                                    <input type="text"   value={veiculo.modelo} onChange={(e) => setVeiculo({ ...veiculo, modelo: e.target.value})}/>
+                                </label>
+
+                                <label className="form-label-editar">
+                                    Ano:
+                                    <input type="text" value={veiculo.ano} onChange={(e) => setVeiculo({ ...veiculo, ano: e.target.value})}/>
+                                </label>
+                            </div>
+                        
+                            <div  className="editar-grupo">
+                                <label className="form-label-editar">
+                                    Portas:
+                                    <input type="text" value={veiculo.portas} onChange={(e) => setVeiculo({ ...veiculo, portas: e.target.value})}/>
+                                </label>
+
+                                <label className="form-label-editar">
+                                    Passageiros:
+                                    <input type="text" value={veiculo.passageiros} onChange={(e) => setVeiculo({ ...veiculo, passageiros: e.target.value})}/>
+                                </label>
+
+                                <label className="form-label-editar">
+                                    Porta Mala:
+                                    <input type="text" value={veiculo.portaMala} onChange={(e) => setVeiculo({ ...veiculo, portaMala: e.target.value})}/>
+                                </label>
+                            </div>
+
+                            <div  className="editar-grupo">
+                                <label className="form-label-editar">
+                                    Automático:
+                                    <input type="text"  value={veiculo.volante} onChange={(e) => setVeiculo({ ...veiculo, volante: e.target.value})}/>
+                                </label>
+
+                                <label className="form-label-editar">
+                                    Ar condicionado:
+                                    <input type="text" value={veiculo.ar} onChange={(e) => setVeiculo({ ...veiculo, ar: e.target.value})}/>
+                                </label>
+
+                                <label className="form-label-editar">
+                                    Direção:
+                                    <input type="text" value={veiculo.direcao} onChange={(e) => setVeiculo({ ...veiculo, direcao: e.target.value})}/>
+                                </label>
+                            </div>
+
+                            <div className="form-button-editar">
+                                <button type="submit">Salvar</button>
+                            </div>
+                        </div>
+                    </form>
+            </div>
         </div>
     );
 }
