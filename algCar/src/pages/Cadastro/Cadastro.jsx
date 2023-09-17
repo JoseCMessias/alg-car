58f1c9c30b9fe032b9fdf42c7ede44544b4cc000
JoseCMessias <josecmessias22@gmail.com>
2023-09-16 22:10:11 -0300

    Criando conta localStorage

--- algCar/src/pages/Cadastro/Cadastro.jsx
+++ algCar/src/pages/Cadastro/Cadastro.jsx
@@ -2,9 +2,19 @@ import React, { useState } from 'react';
 import './Cadastro.css'; 
 import {Link} from 'react-router-dom';
 import logoImg from '../../assets/logo.svg';
+import {useNavigate} from 'react-router-dom';
 
 export const Cadastro = () => {
- 
+  const navigate = useNavigate();
+
+  const [usuario, setUsuario] = useState({ nomeUsuario: '', emailUsuario: '', senhaUsuario: ''})
+
+  const handleCriarConta = (e) => {
+    e.preventDefault()
+    localStorage.setItem('usuario', JSON.stringify(usuario));
+    navigate('/login');
+  }
+
   return (
     <div>
       <div className="section-cadastro">
@@ -12,51 +22,49 @@ export const Cadastro = () => {
           
         <img src={logoImg} alt="imagem da logo" />
 
-          <form>
+          <form onSubmit={handleCriarConta}>
+
             <div className="inputs-cadastro">
               <input
-                type="text"
-                name="name"
-                placeholder="seu nome"
-                autoFocus
-                required
+                  type="text"
+                  name="name"
+                  placeholder="seu nome"
+                  autoFocus
+                  value={usuario.nomeUsuario}
+                  onChange={(e) => setUsuario({ ...usuario, nomeUsuario: e.target.value })}
+                  required
               />
             </div>
 
             <div className="inputs-cadastro">
               <input
-                type="email"
-                name="email"
-                placeholder="seu email"
-                autoFocus
-                required
+                  type="email"
+                  name="email"
+                  placeholder="seu email"
+                  autoFocus
+                  value={usuario.emailUsuario}
+                  onChange={(e) => setUsuario({ ...usuario, emailUsuario: e.target.value })}
+                  required
               />
             </div>
 
             <div className="inputs-cadastro">
               <input
-                type="password"
-                name="senha"
-                placeholder="sua senha"
-                required
+                  type="password"
+                  name="senha"
+                  placeholder="crie uma senha"
+                  value={usuario.senhaUsuario}
+                  onChange={(e) => setUsuario({ ...usuario, senhaUsuario: e.target.value })}
+                  required
               />
             </div>
 
-            <div className="inputs-cadastro">
-              <input
-                type="password"
-                name="senhaConfirme"
-                placeholder="confirme sua senha"
-                required
-              />
-            </div>
-
-            <input type="submit" value="Criar" />
+            <input type="submit" value="Criar" onClick={handleCriarConta}/>
           </form>
 
           <p>
-            Voltar à página de Login?{' '}
-            <Link to='/login'>Clique aqui!</Link>
+              Voltar à página de Login?{' '}
+              <Link to='/login'>Clique aqui!</Link>
           </p>
         </div>
       </div>
