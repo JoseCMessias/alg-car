94a9979df2fdc07e660e0ad4e98e08b3dd809bfc
JoseCMessias <josecmessias22@gmail.com>
2023-09-19 06:48:24 -0300

    atualização do css e jsx

--- algCar/src/pages/Listar/ListarVeiculos.jsx
+++ algCar/src/pages/Listar/ListarVeiculos.jsx
@@ -1,55 +1,66 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import './ListarVeiculos.css'
+import "./ListarVeiculos.css";
 
 export const ListarVeiculos = ({}) => {
-  const [veiculos, setVeiculos] = useState([]);
+    const [veiculos, setVeiculos] = useState([]);
 
-  useEffect(() => {
-    const storedVeiculos = JSON.parse(localStorage.getItem('veiculos')) || [];
-    setVeiculos(storedVeiculos);
-  }, []);
+    useEffect(() => {
+        const storedVeiculos =
+            JSON.parse(localStorage.getItem("veiculos")) || [];
+        setVeiculos(storedVeiculos);
+    }, []);
 
-  const handleRemoverVeiculo = (index) => {
-    const updateVeiculo = veiculos.filter((_, i) => i !== index);
-    localStorage.setItem('veiculos', JSON.stringify(updateVeiculo));
-    setVeiculos(updateVeiculo);
-  }
+    const handleRemoverVeiculo = (index) => {
+        const updateVeiculo = veiculos.filter((_, i) => i !== index);
+        localStorage.setItem("veiculos", JSON.stringify(updateVeiculo));
+        setVeiculos(updateVeiculo);
+    };
 
-  return (
-    <div className="container-listar-listar">
-      <div className="container-listar">
-        <h2 className="titulo-listar">Listar Veiculos</h2>
-        <ul className="veiculo-ul">
-          {veiculos.map((veiculo, index) => (
-            <li className="veiculo-li" key={index}>
-              <div className="imput-file-listar">
-                {/* Certifique-se de que veiculo.image contenha a URL da imagem */}
-                {veiculo.image && <img src={veiculo.image} alt="" />}
-              </div>
+    return (
+        <div className="container-listar-listar">
+            <div className="container-listar">
+                <h2 className="titulo-listar">Listar Veiculos</h2>
+                <ul className="veiculo-ul">
+                    {veiculos.map((veiculo, index) => (
+                        <li className="veiculo-li" key={index}>
+                            <div className="imput-file-listar">
+                                {/* Certifique-se de que veiculo.image contenha a URL da imagem */}
+                                {veiculo.image && (
+                                    <img src={veiculo.image} alt="" />
+                                )}
+                            </div>
 
-              <div className="descricao-listar">
-                <div className="listar-marca">{veiculo.marca}</div>
-                <div className="listar-modelo">{veiculo.modelo}</div>
-              </div>
+                            <div className="descricao-listar">
+                                <div className="listar-marca">
+                                    {veiculo.marca}
+                                </div>
+                                <div className="listar-modelo">
+                                    {veiculo.modelo}
+                                </div>
+                            </div>
 
-              <div className="veiculo-editar-excluir">
-                <div className="veiculo-editar-listar">
-                  <Link to={`/edit/${index}`}>
-                    <button>Editar</button>
-                  </Link>
-                </div>
+                            <div className="veiculo-editar-excluir">
+                                <div className="veiculo-editar-listar">
+                                    <Link to={`/edit/${index}`}>
+                                        <button>Editar</button>
+                                    </Link>
+                                </div>
 
-                <div className="veiculo-excluir-listar">
-                  <button onClick={() => handleRemoverVeiculo(index)}>
-                    Excluir
-                  </button>
-                </div>
-              </div>
-            </li>
-          ))}
-        </ul>
-      </div>
-    </div>
-  );
-}
+                                <div className="veiculo-excluir-listar">
+                                    <button
+                                        onClick={() =>
+                                            handleRemoverVeiculo(index)
+                                        }
+                                    >
+                                        Excluir
+                                    </button>
+                                </div>
+                            </div>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </div>
+    );
+};
