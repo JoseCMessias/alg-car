6e3ecd95447d71b18c035d3a95030e28c601fe3c
JoseCMessias <josecmessias22@gmail.com>
2023-09-19 06:41:33 -0300

    identação do código

--- algCar/src/pages/Sobre/Sobre.jsx
+++ algCar/src/pages/Sobre/Sobre.jsx
@@ -1,28 +1,29 @@
-import Perfil from '../../assets/perfil.svg'
-import './Sobre.css';
+import Perfil from "../../assets/perfil.svg";
+import "./Sobre.css";
 
 export const Sobre = () => {
-
     return (
-            <div className="body-sobre">
-                <div className='container-sobre'>
-                    <h1 className="titulo-sobre">Sobre mim</h1>
-                    <div className='sobre-sobre'>
-                        <div className="perfil-sobre">
-                            <img src={Perfil} alt="" />
-                            <h2>José Messias</h2>
-                        </div>  
+        <div className="body-sobre">
+            <div className="container-sobre">
+                <h1 className="titulo-sobre">Sobre mim</h1>
+                <div className="sobre-sobre">
+                    <div className="perfil-sobre">
+                        <img src={Perfil} alt="" />
+                        <h2>José Messias</h2>
+                    </div>
 
-                        <div className="text-sobre">
-                            <p >
-                                O site ALG CAR é o resultado de um projeto proposto pelo curso da Alicerce Educação como 
-                                projeto final. Nesse empreendimento, desenvolvemos um sistema voltado para o cliente, 
-                                que oferece funcionalidades essenciais, tais como a capacidade de adicionar, listar, 
-                                editar e excluir informações.
-                            </p>
-                        </div>
+                    <div className="text-sobre">
+                        <p>
+                            O site ALG CAR é o resultado de um projeto proposto
+                            pelo curso da Alicerce Educação como projeto final.
+                            Nesse empreendimento, desenvolvemos um sistema
+                            voltado para o cliente, que oferece funcionalidades
+                            essenciais, tais como a capacidade de adicionar,
+                            listar, editar e excluir informações.
+                        </p>
                     </div>
                 </div>
             </div>
+        </div>
     );
-}
+};
