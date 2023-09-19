09ca33f9920f4aa6276e98f50d15cc0d75c6f9c7
JoseCMessias <josecmessias22@gmail.com>
2023-09-19 06:43:02 -0300

    identação do código

--- algCar/src/pages/Home/Home.jsx
+++ algCar/src/pages/Home/Home.jsx
@@ -1,17 +1,15 @@
-import Carrousel from '../../components/Carrousel/Carrousel.jsx';
-import Veiculos from '../../components/Veiculos/Veiculos.jsx';
-import Oferta from '../../components/Oferta/Ofertas.jsx';
-// import {ListarVeiculos} from '../Listar/ListarVeiculos.jsx';
-
+import Carrousel from "../../components/Carrousel/Carrousel.jsx";
+import Veiculos from "../../components/Veiculos/Veiculos.jsx";
+import Oferta from "../../components/Oferta/Ofertas.jsx";
+import { ListarVeiculos } from "../Listar/ListarVeiculos.jsx";
 
 export const Home = () => {
-
     return (
         <>
-           <Carrousel /> 
-           {/* <ListarVeiculos /> */}
-           <Veiculos />
-           <Oferta />
+            <Carrousel />
+            <Veiculos />
+            <Oferta />
+            {/* <ListarVeiculos /> */}
         </>
-    )
-}
+    );
+};
