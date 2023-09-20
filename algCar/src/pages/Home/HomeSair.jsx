cda5ce2d690ad78133aaac705d658d4822fe06a3
JoseCMessias <josecmessias22@gmail.com>
2023-09-20 16:46:10 -0300

    página de home jsx

--- algCar/src/pages/Home/HomeSair.jsx
+++ algCar/src/pages/Home/HomeSair.jsx
@@ -1,6 +1,7 @@
 import Carrousel from "../../components/Carrousel/Carrousel.jsx";
 import Veiculos from "../../components/Veiculos/Veiculos.jsx";
 import { NavbarSair } from "../../components/Navbar/NavbarSair.jsx";
+import Ofertas from "../../components/Oferta/Ofertas.jsx";
 
 export const HomeSair = () => {
     return (
@@ -8,6 +9,7 @@ export const HomeSair = () => {
             <NavbarSair />
             <Carrousel />
             <Veiculos />
+            <Ofertas />
         </>
     );
 };
